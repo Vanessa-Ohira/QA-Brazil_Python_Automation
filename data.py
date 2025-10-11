4a15b5119da8ffcbef5e225d5f73264bd2286503
vanessa <vanessa.ohira@outlook.com>
2025-10-12 00:46:28 +0200

    URL apagada

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Arquivo com constantes
-URBAN_ROUTES_URL = 'https://cnt-ed762c4b-4ee2-4e39-836b-6c4f14a998a7.containerhub.tripleten-services.com?lng=pt'
+URBAN_ROUTES_URL = ''
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
